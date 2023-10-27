b109f11cf6ca3a44addbe075c13ac856cbc9ce6d
Vishnuvardhan27 <Vishnuvardhanc27@gmail.com>
2023-10-27 05:57:41 +0530

    Fixed Code Smell

--- Lab - Code Smells/Code_smells_example.py
+++ Lab - Code Smells/Code_smells_example.py
@@ -1,10 +1,15 @@
 #Code Smells Example for long method
+def calculate_item_price(item):
+    if item.quantity > 0:
+        if item.price > 100:
+            return item.price * 0.9
+        else:
+            return item.price * 0.95
+    else:
+        return 0
+
 def calculate_total_price(items):
     total = 0
     for item in items:
-        if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+        total += calculate_item_price(item)
     return total
