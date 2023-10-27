01d72dd1ff401ee7c29da45057bec39935d7f7a6
Vishnuvardhan27 <Vishnuvardhanc27@gmail.com>
2023-10-27 06:08:12 +0530

    Fixed Duplication issue

--- Lab - Code Smells/Code_smells_example.py
+++ Lab - Code Smells/Code_smells_example.py
@@ -13,3 +13,12 @@ def calculate_total_price(items):
     for item in items:
         total += calculate_item_price(item)
     return total
+
+
+# In this refactored code:
+
+# 1. `calculate_item_price(item)` is a new function that calculates the price for an individual item based on the quantity and price conditions. This function makes the code more readable and easier to understand.
+
+# 2. `calculate_total_price(items)` still calculates the total price, but it now delegates the item price calculation to the `calculate_item_price` function.
+
+# Breaking down the code into smaller functions not only reduces the method's length but also makes it more modular and easier to maintain.
--- Lab - Code Smells/Duplicate_code_example.py
+++ Lab - Code Smells/Duplicate_code_example.py
@@ -1,14 +1,3 @@
-#Duplicate code example
-def calculate_area(length, width):
-    area = length * width
-    return area
-
-
-def calculate_perimeter(length, width):
-    perimeter = 2 * (length + width)
-    return perimeter
-
-
 def calculate_volume(length, width, height):
     volume = length * width * height
     return volume
@@ -17,10 +6,3 @@ def calculate_volume(length, width, height):
 def calculate_surface_area(length, width, height):
     surface_area = 2 * (length * width + length * height + width * height)
     return surface_area
-
-
-# Duplicate code: calculate_area and calculate_perimeter functions have similar calculations.
-def calculate_area_perimeter(length, width):
-    area = length * width
-    perimeter = 2 * (length + width)
-    return area, perimeter
